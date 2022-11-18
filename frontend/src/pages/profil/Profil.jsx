57f7cb55ffd1806fb1906e69d00432e9dbdc5f79
wpacyniak <nerdyronnie@gmail.com>
2022-11-18 22:36:11 +0100

    Start work at profil page

--- frontend/src/pages/profil/Profil.jsx
+++ frontend/src/pages/profil/Profil.jsx
@@ -1,6 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { Wrapper } from "./styles";
+import {
+  Wrapper,
+  Login,
+  Email,
+  Number,
+  Logo,
+  FullLogoWrapper,
+  LogoLabel,
+  AmountText,
+  LogoWrapper,
+  Row,
+  TextWrapper,
+} from "./styles";
 import { Header } from "../../components/header/Header";
 import { Footer } from "../../components/footer/Footer";
 
@@ -8,11 +20,48 @@ import { useStore } from "../../Store";
 
 export const Profil = () => {
   const navigate = useNavigate();
-  const { dispatch } = useStore();
+  const { state, dispatch } = useStore();
+  const currentYear = new Date().getFullYear();
+
+  useEffect(() => {
+    if (!state.user || !state.auth_token) {
+      dispatch({
+        type: "SET_USER",
+        payload: JSON.parse(localStorage.getItem("user")),
+      });
+      dispatch({
+        type: "SET_AUTH_TOKEN",
+        payload: JSON.parse(localStorage.getItem("token")),
+      });
+    } else {
+      //   pobranie danych o przeczytanych ksiazkach + stronach
+    }
+  }, [state.user, state.token]);
 
   return (
     <Wrapper>
       <Header />
+      <Row>
+        <FullLogoWrapper>
+          <LogoLabel>Przeczytane w {currentYear} roku:</LogoLabel>
+          <LogoWrapper>
+            <Logo
+              src={require("../../resources/images/icon_book.png")}
+              alt="logo"
+            />
+            <Number>10</Number>
+          </LogoWrapper>
+          <LogoLabel>książek</LogoLabel>
+        </FullLogoWrapper>
+        <TextWrapper>
+          <Login>{state.user.username}</Login>
+          <Email>{state.user.email}</Email>
+          <AmountText>
+            W tym roku przeczytałeś o 10% więcej niż w poprzednim.
+          </AmountText>
+        </TextWrapper>
+      </Row>
+
       <Footer />
     </Wrapper>
   );
--- frontend/src/pages/profil/styles.js
+++ frontend/src/pages/profil/styles.js
@@ -18,4 +18,78 @@ const Wrapper = styled.div`
   }
 `;
 
-export { Wrapper };
+const Row = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  gap: 100px;
+`;
+
+const TextWrapper = styled.div``;
+
+const Email = styled.h3`
+  font-family: "DM Sans";
+  font-style: normal;
+  font-weight: 300;
+`;
+
+const Login = styled.h2`
+  font-family: "DM Sans";
+  font-style: normal;
+`;
+
+const Logo = styled.img`
+  width: 100%;
+`;
+
+const LogoWrapper = styled.div`
+  position: relative;
+  text-align: center;
+  width: 200px;
+`;
+
+const LogoLabel = styled.p`
+  font-family: "DM Sans";
+  font-style: normal;
+  text-transform: uppercase;
+  margin-bottom: 10px;
+  font-weight: 500;
+`;
+
+const Number = styled.p`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  font-size: 140px;
+  font-weight: bold;
+`;
+
+const FullLogoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const AmountText = styled.p`
+  margin-top: 80px;
+  font-size: 18px;
+  width: 240px;
+  font-family: "DM Sans";
+  font-style: normal;
+`;
+
+export {
+  Wrapper,
+  Email,
+  AmountText,
+  FullLogoWrapper,
+  TextWrapper,
+  LogoLabel,
+  Login,
+  Number,
+  LogoWrapper,
+  Logo,
+  Row,
+};
